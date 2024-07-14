decc1bf5dafd5d6cf7420247999155b5dbb5d7b0
julietvl <julietgorbunova@gmail.com>
2024-07-14 23:47:18 +0300

    Задание про подсчет итераций функций

---
+++ module_3_1.py
@@ -0,0 +1,29 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string_lower = string.lower()
+
+    list_to_search_lower = []
+    for item in list_to_search:
+        list_to_search_lower.append(item.lower())
+
+    return string_lower in list_to_search_lower
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
